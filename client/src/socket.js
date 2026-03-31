b896e1c3234719e8ebb92da8b6ca3ad98c3d8466
Kaushik363 <kaushiksunkari291104@gmail.com>
2026-03-31 16:22:24 +0530

    fix socket for production

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,11 +1,12 @@
 import {io} from 'socket.io-client'; 
 
-export const initSocket=async ()=>{
+export const initSocket = async () => {
     const options = {
         forceNew: true,
         reconnectionAttempts: Infinity,
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(process.env.REACT_APP_BACKEND_URL, options);
+    const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || window.location.origin;
+    return io(BACKEND_URL, options);
 }
