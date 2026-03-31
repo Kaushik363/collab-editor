ac45e0a4636fc2c0e66b09af2c080152ce1224d3
Kaushik363 <kaushiksunkari291104@gmail.com>
2026-03-31 17:51:53 +0530

    fix socket url for production

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
-import {io} from 'socket.io-client'; 
+import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
     const options = {
@@ -9,4 +9,4 @@ export const initSocket = async () => {
     };
     const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || window.location.origin;
     return io(BACKEND_URL, options);
-}
+};
